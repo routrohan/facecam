65d8b59ebdeb1c9fbf9402040158655e383f3847
Rohan Rout <rohan.rout98@gmail.com>
2020-08-12 03:18:20 +0530

    facebox and routes added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navigation from './components/Navigation/Navigation'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Signin from './components/Signin/Signin'
+import Register from './components/Register/Register'
 import Rank from './components/Rank/Rank'
 import './App.css';
 import Clarifai from 'clarifai';
@@ -30,9 +32,33 @@ class App extends Component {
     super ();
     this.state = {
       input:'',
-      imageUrl: ''
+      imageUrl: '',
+      box:{},
+      route: 'signin',
+      isSignedIn: false
     }
     // this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  calculateFaceLocation = (data)=>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width);
+    const height = Number(image.height);
+    //console.log(width,height)
+
+    return {
+      leftCol : clarifaiFace.left_col*width,
+      topRow : clarifaiFace.top_row*height,
+      rightCol : width - (clarifaiFace.right_col * width),
+      bottomRow : height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box:box})
   }
 
   onInputChange = (event) =>{
@@ -42,15 +68,20 @@ class App extends Component {
   onSubmit = ()=>{
     this.setState({imageUrl:this.state.input})
     console.log("click")
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-    function(response) {
-      // do something with response
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-    },
-    function(err) {
-      // there was an error
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+  
+  }
+
+  onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState({isSignedIn:false})
     }
-  );
+    else if(route === 'home'){
+      this.setState({isSignedIn:true})
+    }
+    this.setState({route:route})
   }
 
   render(){
@@ -60,11 +91,28 @@ class App extends Component {
               params={particleOptions}
               
             />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm  onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
-      <FaceRecognition imageUrl={this.state.imageUrl}/>
+      <Navigation isSignedIn={this.state.isSignedIn} onRouteChange = {this.onRouteChange} />
+
+      {
+        this.state.route === 'home' ? 
+        <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm  onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+        </div>
+        
+        : (
+          this.state.route === 'signin'? 
+          <Signin onRouteChange = {this.onRouteChange}/> 
+            :
+          <Register onRouteChange = {this.onRouteChange}/>
+        )
+
+        
+        
+        
+      }
     </div>
   );
 
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
-function FaceRecognition({imageUrl}){
+import './FaceRecognition.css'
+function FaceRecognition({imageUrl, box}){
     return(
         <div className="center ma">
             <div className='absolute mt2'>
-                <img alt="" src={imageUrl} width='400px' height='auto'/>
+                <img id = "inputimage" alt="" src={imageUrl} width='400px' height='auto'/>
+                <div className='bounding-box' style={{top:box.topRow, right:box.rightCol, bottom: box.bottomRow, left:box.leftCol}}></div>
             </div>
         </div>
     )
