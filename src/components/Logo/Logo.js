a320450c5c7a5fd994a98f1b231f51104a692d82
Rohan Rout <rohan.rout98@gmail.com>
2020-08-13 16:47:23 +0530

    final changes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import Tilt from 'react-tilt'
-import './Logo.css'
-import brain from './brain.png'
-function Logo(){
-    return(
-        <div className='ma4 nt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-            <div className="Tilt-inner pa3"><img alt='Logo' style={{paddingTop:'5px'}} src={brain}/> </div>
-            </Tilt>
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
         </div>
-    )
+      </Tilt>
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,16 @@
-import React from 'react'
-function Rank(){
-    return (<div>
+import React from 'react';
 
-        <div className="while f3">
-            {'Rohan your current rank is...'}
-        </div>
-
-        <div className="while f1">
-            {'#5'}
-        </div>
-        
-    </div>)
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1'>
+        {entries}
+      </div>
+    </div>
+  );
 }
 
-export default Rank
+export default Rank;
